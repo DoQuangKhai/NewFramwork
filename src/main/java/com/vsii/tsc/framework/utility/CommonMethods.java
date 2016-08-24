a49aef434654bc6fc0bc11e0896d208c8246503f
Do Quang Khai <Do Quang Khai@DoQuangKhai-PC>
2016-08-24 23:49:50 +0700

    New

--- src/main/java/com/vsii/tsc/framework/utility/CommonMethods.java
+++ src/main/java/com/vsii/tsc/framework/utility/CommonMethods.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.vsii.tsc.project.pages.method.JiraLoginPageMethod;
-import com.vsii.tsc.project.pages.method.LoginPageMethod;
+import com.vsii.tsc.project.JiraLoginPageMethod;
+import com.vsii.tsc.project.LoginPageMethod;
 
 public class CommonMethods {
   
